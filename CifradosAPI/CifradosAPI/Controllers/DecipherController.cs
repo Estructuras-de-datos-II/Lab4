17a6fcfbaab339e08e2b6ff2b437841790009f09
martinbechfurlan <martinbechfurlan@gmail.com>
2021-10-27 11:12:39 -0600

    final main

---
+++ CifradosAPI/CifradosAPI/Controllers/DecipherController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Encryptor;
+
+namespace Cifrados.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DecipherController : ControllerBase
+    {
+        
+        [HttpPost]
+        public IActionResult postdecipher([FromForm] IFormFile[] files, [FromForm] string key)
+        {
+            byte[] buffer = new byte[0];
+            byte[] bufferfinal = new byte[0];
+
+            Encryptator descifrador = null;
+
+            IFormFile file = files[0];
+            string nombrearchiv = file.FileName;
+            string nombrearchivofinal = nombrearchiv.Split(".").First();
+
+            if (file == null)
+            {
+                return BadRequest(new string[] { "El valor -File- es inválido" });
+            }
+            
+            else if (Path.GetExtension(file.FileName) == ".zz")
+            {
+                int llave = Convert.ToInt32(key);
+                descifrador = new ZigZagEncryptator();
+                using (MemoryStream archivotexto = new MemoryStream())
+
+                    try
+                    {
+
+                        var bytesarray = archivotexto.ToArray();
+                        file.CopyToAsync(archivotexto);
+                        bufferfinal = descifrador.decrypt(archivotexto.ToArray(), llave);
+                        generarArchivo_txt(bufferfinal, nombrearchivofinal, ".txt");
+                    }
+                    catch (Exception)
+                    {
+                        return StatusCode(500);
+                    }
+
+            }
+            else if (Path.GetExtension(file.FileName) == ".csr")
+            {
+                byte[] bytellave = Encoding.ASCII.GetBytes(key);
+                descifrador = new CesarEncrypt();
+                using (MemoryStream archivotexto = new MemoryStream())
+
+                    try
+                    {
+
+                        var bytesarray = archivotexto.ToArray();
+                        file.CopyToAsync(archivotexto);
+                        bufferfinal = descifrador.decrypt(archivotexto.ToArray(), bytellave);
+                        generarArchivo_txt(bufferfinal, nombrearchivofinal, ".txt");
+                    }
+                    catch (Exception)
+                    {
+                        return StatusCode(500);
+                    }
+
+            }
+            else if (Path.GetExtension(file.FileName) == ".rt")
+            {
+                int llave = Convert.ToInt32(key);
+                descifrador = new RutaEncryptator();
+                using (MemoryStream archivotexto = new MemoryStream())
+
+                    try
+                    {
+                        var bytesarray = archivotexto.ToArray();
+                        file.CopyToAsync(archivotexto);
+                        bufferfinal = descifrador.decrypt(archivotexto.ToArray(), llave);
+                        generarArchivo_txt(bufferfinal, nombrearchivofinal, ".txt");
+                    }
+                    catch (Exception e)
+                    {
+                        return StatusCode(500);
+                    }
+            }
+            return Ok();
+        }
+
+        public void generarArchivo_txt(byte[] datos, string name, string extension)
+        {
+            string fileName = "../../Archivos/" + name + extension;
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
+            {
+
+                fileStream.Write(datos, 0, datos.Length);
+            }
+
+        }
+    }
+}
---
+++ Encryptor/ZigZagEncryptator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryptor
+{
+    public class ZigZagEncryptator : Encryptator
+    {
+        public byte[] decrypt(byte[] dataToDencrypt, int key)
+        {
+            int rest = dataToDencrypt.Length % 2;
+            byte[] decryptedData = new byte[dataToDencrypt.Length];
+            for(int i = 0; i < (dataToDencrypt.Length / 2 + rest); i++)
+            {
+                decryptedData[i * 2] = dataToDencrypt[i];
+            }
+            int j = 0;
+            for (int i = (dataToDencrypt.Length) / 2 + rest; i < dataToDencrypt.Length; i++)
+            {
+                decryptedData[j * 2 + 1] = dataToDencrypt[i];
+                j++;
+            }
+            return decryptedData;
+        }
+
+        public byte[] decrypt(byte[] dataToDencrypt, byte[] key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] encrypt(byte[] dataToEncrypt, int key)
+        {
+            byte[] leftData;
+            if (dataToEncrypt.Length % 2 == 1)
+            {
+                leftData = new byte[(dataToEncrypt.Length / 2 + 1)];
+            }
+            else
+            {
+                leftData = new byte[(dataToEncrypt.Length / 2)];
+            }
+            byte[] rightData = new byte[(dataToEncrypt.Length / 2)];
+
+            int i = 0;
+            foreach (byte b in dataToEncrypt)
+            {
+
+                    if(i % 2 == 0)
+                    {
+                        leftData[i / 2] = b;
+                    }
+                    else
+                    {
+                        rightData[i / 2] = b;
+                    }
+                    i++;
+            }
+            byte[] encryptedData = new byte[leftData.Length + rightData.Length];
+            leftData.CopyTo(encryptedData, 0);
+            //Console.WriteLine(encryptedData.Length);
+            //Console.WriteLine(leftData.Length + rightData.Length);
+            rightData.CopyTo(encryptedData, leftData.Length);
+
+            var sb = new StringBuilder("new byte[] { ");
+            foreach (var b in rightData)
+            {
+                sb.Append(b + ", ");
+            }
+            sb.Append("}");
+            //Console.WriteLine(sb.ToString());
+
+            return encryptedData;
+        }
+
+        public byte[] encrypt(byte[] dataToEncrypt, byte[] key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
